cebfedebd5d829e7dbf2921083561249ca766574
Sukrit Rerk-u-cote <sukrit_love@Sukrits-MacBook-Pro.local>
2023-11-09 10:31:12 +0700

    Write automated tests

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -155,7 +155,7 @@ fun TipTimeLayout() {
 }
 
 // -> Functionality
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String {
+fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String {
     var tip = tipPercent / 100 * amount
     if (roundUp) {
         tip = kotlin.math.ceil(tip)
@@ -180,7 +180,7 @@ fun EditNumberField(
         onValueChange = onValueChange,
         keyboardOptions = keyboardOptions,
         label = { Text(stringResource(label)) },
-        leadingIcon = { Icon(painter = painterResource(id = leadingIcon), null)},
+        leadingIcon = { Icon(painter = painterResource(id = leadingIcon), null) },
         singleLine = true
 
     )
